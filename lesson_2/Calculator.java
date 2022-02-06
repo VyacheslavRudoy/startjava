5c8ee5f545cc103f4da84650bb26388fee436759
VyacheslavRudoy <97558285+VyacheslavRudoy@users.noreply.github.com>
2022-02-06 21:08:50 +0300

    Create Calculator.java

---
+++ lesson_2/Calculator.java
@@ -0,0 +1,35 @@
+public class Calculator {
+
+    private int result;
+
+        public void calculate(int a, int b, char symbol) {
+        if (a % 1 == 0 && b % 1 == 0 && a > 0 && b > 0) {
+            switch (symbol) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                case '^':
+                    result = 1;
+                    for (int i = 1; i <= b; i++) {
+                        result *= a;
+                    }
+                    break;
+                case '%':
+                    result = a % b;
+            }
+        }
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
