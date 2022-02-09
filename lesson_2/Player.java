f1394219993e2c1ad7819570b85ca579483952b8
VyacheslavRudoy <97558285+VyacheslavRudoy@users.noreply.github.com>
2022-02-09 21:48:45 +0300

    Update Player.java

--- lesson_2/Player.java
+++ lesson_2/Player.java
@@ -1,29 +1,14 @@
-import java.util.Scanner;
-
 public class Player {
     private String name;
 
-    public Player() {
-        name = "No name";
-    }
-
     public Player(String name) {
-        if (name.equals("")) {
+        if (name.isEmpty()) {
             name = "No name";
         }
-
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
     public String toString() {
-        return String.format(name);
+        return name;
     }
 }
