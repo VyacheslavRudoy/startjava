3deba037a3b59ce9d864f0b996d3e799e76c1ea9
VyacheslavRudoy <97558285+VyacheslavRudoy@users.noreply.github.com>
2022-02-11 20:05:23 +0300

    Update GuessNumber.java

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -2,45 +2,37 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-    public static int guess() {
-        Scanner scanner = new Scanner(System.in);
-        int answer = scanner.nextInt();
-        return answer;
+    public GuessNumber(Player player1, Player player2) {
     }
 
     public void start(Player player1, Player player2) {
-        int random = (int) (Math.random() * 101);
+        int randomNumber = (int) (Math.random() * 101);
         int user1Number = 0;
         int user2Number = 0;
 
         while (true) {
             System.out.println("Игрок " + player1 + ", ваш ход:");
-            user1Number = GuessNumber.guess();
-            if (user1Number < random) {
+            Scanner scanner = new Scanner(System.in);
+            user1Number = scanner.nextInt();
+            if (user1Number < randomNumber) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
-            } else if (user1Number > random) {
+            } else if (user1Number > randomNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            } else if (user1Number == random) {
+            } else if (user1Number == randomNumber) {
                 System.out.println("Поздравляю, " + player1 + " угадал число!");
                 break;
             }
 
-
             System.out.println("Игрок " + player2 + ", ваш ход:");
-            user2Number = GuessNumber.guess();
-            if (user2Number < random) {
+            user2Number = scanner.nextInt();
+            if (user2Number < randomNumber) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
-            } else if (user2Number > random) {
+            } else if (user2Number > randomNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            } else if (user2Number == random) {
+            } else if (user2Number == randomNumber) {
                 System.out.println("Поздравляю, " + player2 + " угадал число!");
                 break;
             }
         }
     }
 }
-
-
-
-
-
