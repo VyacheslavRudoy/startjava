b0ea9ffdfdacc1937f96e57619a74da05179a9b4
VyacheslavRudoy <97558285+VyacheslavRudoy@users.noreply.github.com>
2022-02-11 21:21:29 +0300

    Update GuessNumberTest.java

--- lesson_2/GuessNumberTest.java
+++ lesson_2/GuessNumberTest.java
@@ -6,16 +6,14 @@ public class GuessNumberTest {
         System.out.println("Введите имя игрока 1:");
         Scanner scanner = new Scanner(System.in);
         String nameOne = scanner.nextLine();
-        Player player1 = new Player(nameOne);
 
         System.out.println("Введите имя игрока 2:");
         String nameTwo = scanner.nextLine();
-        Player player2 = new Player(nameTwo);
 
         String nextRound = "";
         while (!nextRound.equals("no")) {
-            GuessNumber game = new GuessNumber(player1, player2);
-            game.start(player1, player2);
+            GuessNumber game = new GuessNumber(nameOne, nameTwo);
+            game.start();
             nextRound = "";
 
             while (!(nextRound.equals("no") || nextRound.equals("yes"))) {
